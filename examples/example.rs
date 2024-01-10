0ae49c07d0bf24d533a9b341162500f83c028a73
realcool <547367379@qq.com>
2024-01-10 13:35:54 +0800

    fix (World cant exist with ResMut do something nextState)

--- examples/example.rs
+++ examples/example.rs
@@ -7,32 +7,50 @@ use classes::*;
 #[derive(Component, Debug)]
 enum UiId {
     HiyaButton,
-    HowdyButton
+    HowdyButton,
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))    // Nearest neighbor scaling for pixel graphics!
-        .add_systems(Startup, startup)
+        .add_state::<GameState>()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // Nearest neighbor scaling for pixel graphics!
+        .add_systems(Startup, setup_scale)
+        .add_systems(OnEnter(GameState::Play), startup)
         .add_systems(Update, handle_interactions)
         .run();
 }
 
-fn startup(mut commands: Commands, world: &World, mut scale: ResMut<UiScale>) {
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum GameState {
+    #[default]
+    Loading,
+    Play,
+}
 
+fn setup_scale(
+    mut commands: Commands,
+    mut scale: ResMut<UiScale>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
     // Obligatory camera
     commands.spawn(Camera2dBundle::default());
-    scale.0 = 2.0;
+    scale.0 = 2.;
+    next_state.set(GameState::Play);
+}
 
+fn startup(mut commands: Commands, world: &World) {
     // Spawns ui and gathers entity ids
     let mut hiya = None;
     let mut howdy = None;
-    root(c_root, world, &mut commands, |p| {                                  // Spawns the root NodeBundle. AssetServer gets propagated.
-        node((c_half, c_green), p, |p| {                                        // Spawns the left pane as a NodeBundle.
-            text("This is the left pane!", c_text, c_pixel, p);                 // Spawns a TextBundle.
+    root(c_root, world, &mut commands, |p| {
+        // Spawns the root NodeBundle. AssetServer gets propagated.
+        node((c_half, c_green), p, |p| {
+            // Spawns the left pane as a NodeBundle.
+            text("This is the left pane!", c_text, c_pixel, p); // Spawns a TextBundle.
             text("Do you like it?", c_text, c_pixel, p);
-            text_button("Hiya", c_button_left, c_pixel, p).set(&mut hiya);      // Spawns a ButtonBundle with a TextBundle child in the middle. Convenience widget.
-            grid(6, 6, c_grid, p, |p, _row, _col| {                             // Spawns a NodeBundle container with a NodeBundle for each cell (6x6).
+            text_button("Hiya", c_button_left, c_pixel, p).set(&mut hiya); // Spawns a ButtonBundle with a TextBundle child in the middle. Convenience widget.
+            grid(6, 6, c_grid, p, |p, _row, _col| {
+                // Spawns a NodeBundle container with a NodeBundle for each cell (6x6).
                 image(c_inv_slot, p);
             });
             text("Le grid", c_text, c_pixel, p);
@@ -46,12 +64,8 @@ fn startup(mut commands: Commands, world: &World, mut scale: ResMut<UiScale>) {
 
     // Inserts marker components into the gathered entities.
     // Useful when you need to interact with specific entities in the UI.
-    commands
-        .entity(hiya.unwrap())
-        .insert(UiId::HiyaButton);
-    commands
-        .entity(howdy.unwrap())
-        .insert(UiId::HowdyButton);
+    commands.entity(hiya.unwrap()).insert(UiId::HiyaButton);
+    commands.entity(howdy.unwrap()).insert(UiId::HowdyButton);
 }
 
 // ---------- System that handles interactions with ui ----------
